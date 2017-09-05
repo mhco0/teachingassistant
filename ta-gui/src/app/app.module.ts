dae32caad66cf5bdd305e06649cd1ea3b1753198
pauloborba <phmb@cin.ufpe.br>
2017-09-05 19:24:27 -0300

    modularizando o componente principal

--- ta-gui/src/app/app.module.ts
+++ ta-gui/src/app/app.module.ts
@@ -1,20 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MetasComponent } from './metas.component';
+<<<<<<< HEAD
+=======
+import { AlunosComponent } from './alunos.component';
+>>>>>>> 3d64e16... modularizando o componente principal
 import { AlunoService } from './aluno.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     MetasComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
+=======
+    MetasComponent,
+    AlunosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'metas',
+        component: MetasComponent
+      },
+      {
+        path: 'alunos',
+        component: AlunosComponent
+      }
+    ])
+>>>>>>> 3d64e16... modularizando o componente principal
   ],
   providers: [AlunoService],
   bootstrap: [AppComponent]
