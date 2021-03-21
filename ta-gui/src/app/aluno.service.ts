71c30cf3bfdc98faa76992c317a53015e7c1d40f
mhco0 <mhco@cin.ufpe.br>
2021-03-21 16:26:26 -0300

    finishing question 27

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -2,12 +2,15 @@ import { Aluno } from './aluno';
 
 export class AlunoService {
   alunos: Aluno[] = [];
-  gravar(aluno: Aluno): Aluno | null {
+  gravar(aluno: Aluno): Aluno {
     var result = null;
-    if (!this.alunos.find(a => a.cpf == aluno.cpf)) {
+    if (this.cpfNaoCadastrado(aluno.cpf)) {
       this.alunos.push(aluno);
       result = aluno;
     }
     return result;
+  }
+  cpfNaoCadastrado(cpf: string): boolean {
+     return !this.alunos.find(a => a.cpf == cpf);
   }
 }
